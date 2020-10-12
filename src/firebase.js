228ed56857e5e85d56ff0ef0c1de1c5030449718
lazalaki <lakierbes@gmail.com>
2020-10-12 20:57:21 +0200

    Header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDb4vth5-SoRDd3jouUvW-c-f6VZC-pEfE",
+  authDomain: "clone-2f8b4.firebaseapp.com",
+  databaseURL: "https://clone-2f8b4.firebaseio.com",
+  projectId: "clone-2f8b4",
+  storageBucket: "clone-2f8b4.appspot.com",
+  messagingSenderId: "984378651150",
+  appId: "1:984378651150:web:cbb1f0f77891d79565c25f",
+  measurementId: "G-HGLZWMJWT0",
+};
